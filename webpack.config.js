7b0094bcbe6ff16b36596eabb6a5d6a31ac8e19c
Your Name <your-email@example.com>
2025-06-03 21:14:34 +0200

    Add webpack configuration and polyfills for Node.js core modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(
@@ -25,6 +26,24 @@ module.exports = async function (env, argv) {
           to: path.resolve(__dirname, 'assets', 'favicon.png'),
         },
       ],
+    })
+  );
+
+  // Add polyfills for Node.js core modules
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
+    "crypto": require.resolve("crypto-browserify"),
+    "stream": require.resolve("stream-browserify"),
+    "path": require.resolve("path-browserify"),
+    "fs": false,
+    "os": require.resolve("os-browserify/browser"),
+    "buffer": require.resolve("buffer/"),
+  };
+
+  // Add Buffer polyfill
+  config.plugins.push(
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
     })
   );
 
